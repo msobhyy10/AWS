f4cbcfa741462d9ef8350813e1f8b2393161b06c
mosobhyy <mosobhyy74@gmail.com>
2021-09-15 01:01:03 +0200

    Uploading package to pypi

---
+++ 5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_aws_probability_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['udacity_aws_probability_dist'],
+      author = 'Muhamed Sobhy',
+      author_email = 'mosobhyy74@gmail.com',
+      zip_safe=False)
---
+++ 5_exercise_upload_to_pypi/udacity_aws_probability_dist/README.md
@@ -0,0 +1,11 @@
+# aws-probability-dist package
+
+Summary of tha package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
+
+pip install aws-probability-dist
